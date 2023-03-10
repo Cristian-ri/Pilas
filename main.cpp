1f641f0b37a36b9fb52002791f59f71dbbf2c992
CristianRivera919 <99370497+Cristian-ri@users.noreply.github.com>
2023-03-10 11:35:40 -0600

    Pruebas.
    Se realizo pruebas para verificar que todo funcionara.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,13 @@ int main(int argc, char** argv)
 	
 	p1.pushFront(8);
 	p1.pushFront(5);
+	p1.pushFront(10);
+	p1.pushFront(15);
+	p1.pushFront(2);
+	p1.printPilas();
+	
+	cout<<"******************"<<endl;
+	p1.popFront();
 	p1.printPilas();
 	cout<<"******************"<<endl;
 	p1.popFront();
